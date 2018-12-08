8eebd1dbf10e5d33d366159c5d3a90eb2190a459
BenjaminHk <44491689+BenjaminHk@users.noreply.github.com>
2018-12-08 20:46:49 +0800

    Update 课堂作业1.py

--- 168206107/课堂作业1.py
+++ 168206107/课堂作业1.py
@@ -1,54 +1,96 @@
 from collections import deque
-def search(start,end,*ad):
+class dd():
+    def search(self,start,end,*ad):
+        adict = list(ad)
+        graph = {}
+        adict.insert(0,start)
+        for dot in range(0,len(adict)):
+            graph[adict[dot]] = []
+            for neighbour in range(0,len(adict)):
+                xiangsidu = 0
+                for i in range(0,len(adict[dot])):
+                    if adict[dot][i] == adict[neighbour][i]:
+                        xiangsidu += 1 
+                    if xiangsidu == len(adict[dot])-1:
+                        graph[adict[dot]].append(adict[neighbour])
+        for key in graph.keys():
+            graph[key] = list(set(graph[key]))
+        for key,value in graph.items():
+            if start in value:
+                graph[key].remove(start)  
+            if key in value:
+                graph[key].remove(key)
+        search_queue = deque()
+        search_queue += graph[start]
+        searched = [start]
+        line = []
+        while search_queue :
+            person = search_queue.popleft()
+            if person not in searched:
+                if self.person_is(person,end):
+                    while person != start:
+                        for key,value in graph.items():
+                            if person in value and key in searched :
+                                line.append(person)
+                                person = key
+                    line.reverse()
+                    return line
+                else:
+                    search_queue += graph[person]
+                    searched.append(person)
+        return False
+    def person_is(self,this_word,end):
+        if this_word == end:
+            return True
+        for letter in range(0,len(this_word)):
+            panduan = this_word
+            for i in range(0,27):
+                panduan = panduan[0:letter] + chr(97+i) +panduan[letter+1:]
+                if panduan == end:
+                    return True
+        return False
+
+a = dd()
+print(a.search('hit','cog','hig','cig','dog','log','hot'))
+print(a.search('adf','skc','err','adf','cvj','adc','sdc'))
+
+
+
+  #另一种解法 
+    def solution(start,end,*ad):
     adict = list(ad)
-    graph = {}
-    adict.insert(0,start)
-    for dot in range(0,len(adict)):
-        graph[adict[dot]] = []
-        for neighbour in range(0,len(adict)):
+    adict.append(start)
+    word_value_table = {}
+    for word in adict:
+         word_value_table[word] = []
+         for i in range(0,len((word))):
+             word_value_table[word].append(ord(word[i]))
+    line =  []
+    word = end
+    searched = []
+    while word != start:
+        for key in word_value_table.keys():
             xiangsidu = 0
-            for i in range(0,len(adict[dot])):
-                if adict[dot][i] == adict[neighbour][i]:
-                    xiangsidu += 1 
-                if xiangsidu == len(adict[dot])-1:
-                    graph[adict[dot]].append(adict[neighbour])
-    for key,value in graph.items():
-        if key in value:
-            graph[key].remove(key)
-        if start in value:
-            graph[key].remove(start)
-    for key in graph.keys():
-        graph[key] = list(set(graph[key]))
-    search_queue = deque()
-    search_queue += graph[start]
-    searched = [start]
-    line = []
-    while search_queue :
-        person = search_queue.popleft()
-        if person not in searched:
-            if person_is(person,end):
-                while person != start:
-                    for key,value in graph.items():
-                        if person in value and key in searched:
-                            line.append(person)
-                            person = key
+            xiangsidu_with_qidian = 0
+            for i in range(0,len(word)):
+                if word_value_table[key][i] == ord(start[i]):
+                    xiangsidu_with_qidian += 1
+                if word_value_table[key][i] == ord(word[i]):
+                    xiangsidu += 1
+            if xiangsidu_with_qidian >= 2:
+                line.remove(end)
                 line.reverse()
                 return line
-            else:
-                search_queue += graph[person]
-                searched.append(person)
-        
-    return False
-def person_is(this_word,end):
-    if this_word == end:
-        return True
-    for letter in range(0,len(this_word)):
-        panduan = this_word
-        for i in range(0,27):
-            panduan = panduan[0:letter] + chr(97+i) +panduan[letter+1:]
-            if panduan == end:
-                return True
-    return False
-print(search('hit','cog','lot','dot','dog','log','hot'))  
-        
-   
+            if xiangsidu >= 2:
+                line.append(word)
+                word = key
+                word_value_table.pop(key)
+                break
+            if set(searched)==set(adict):
+                return False
+            searched.append(key)
+
+print(solution('hit','cog','lot','dot','fog','log','hot'))
+
+
+
